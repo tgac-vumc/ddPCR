032be38acc82cdd249b28729169a04b43a9c971a
Hendrik_F van_Essen <h.vanessen@vumc.nl>
2015-12-02 16:05:02 +0100

    changed: get.well() added: get.plate.wells()

--- ddPCR.R
+++ ddPCR.R
@@ -279,10 +279,31 @@ library(dpcR)
     result <- ((-log(negCount/Count)/vDroplet))*1000*volume
     return(result)
   }
-  get.well <- function(x)
+  get.plate.wells <- function(prefix=NULL,suffix=NULL)
   {
-    x <- strsplit(as.character(x), split = "_")[[1]][2]
+    result <- rep(NA, 96)
     
+    numbers <- c("01","02","03","04","05","06","07","08","09","10","11","12")
+    start <- 1
+    for(i in 1:8)
+    {
+      result[start:(start+11)] <- paste(LETTERS[i],numbers, sep="")
+      start <- start + 12
+    }
+    if(class(prefix) != "NULL")
+    {
+      result <- paste(prefix,result,sep="")
+    }
+    if(class(suffix) != "NULL")
+    {
+      result <- paste(result,suffix,sep="")
+    }
+    return(result)
+  }
+  get.well <- function(input.file)
+  {
+    x <- strsplit(as.character(input.file), split = "_")[[1]]
+    x <- x[x %in% get.plate.wells()]
     return(x)
   }
   get.statistics <- function(x,sample="",input.file="",target="",breakpoints=NULL)
