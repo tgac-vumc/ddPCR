1c44c144f268ce66caea47648d5d542c3af6fccd
Dirk van Essen <h.vanessen@vumc.nl>
2016-05-03 00:56:51 +0200

    changes in thresholdMatrix file - unfinished

--- createThresholdMatrix.R
+++ createThresholdMatrix.R
@@ -4,3 +4,19 @@ createThresholdMatrix <- function()
          dimnames = list(c("minOutlier","maxOutlier","threshold","thresholdMeanStDev","maxAmplitude","minRain","maxRain"), c("ch1","ch2")))
   return(result)
 }
+addThresholdData <- function(x = NULL, add, type = '')
+{
+  
+  if (class(result != "matrix"))
+  {
+    result <- matrix(data = NA, nrow = 7, ncol = 2, 
+                     dimnames = list(c("minOutlier","maxOutlier","threshold","thresholdMeanStDev","maxAmplitude","minRain","maxRain"), c("ch1","ch2")))
+    
+  }
+  dimnames = list(c("minOutlier","maxOutlier","threshold","thresholdMeanStDev","maxAmplitude","minRain","maxRain"), c("ch1","ch2")))
+
+}
+# [ ] - create new function that will create matrix or add settings to it.
+# some fixed settings
+# able to add own settings as well
+# 
