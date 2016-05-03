4a392f0d6cfe6f34575c64d7749a4baaaabef386
HF van Essen <h.vanessen@vumc.nl>
2016-05-03 16:55:18 +0200

    added pipeline folder

--- createThresholdMatrix.R
+++ createThresholdMatrix.R
@@ -13,7 +13,7 @@ addThresholdData <- function(x = NULL, add, type = '')
                      dimnames = list(c("minOutlier","maxOutlier","threshold","thresholdMeanStDev","maxAmplitude","minRain","maxRain"), c("ch1","ch2")))
     
   }
-  dimnames = list(c("minOutlier","maxOutlier","threshold","thresholdMeanStDev","maxAmplitude","minRain","maxRain"), c("ch1","ch2")))
+  dimnames = list(c("minOutlier","maxOutlier","threshold","thresholdMeanStDev","maxAmplitude","minRain","maxRain"), c("ch1","ch2"))
 
 }
 # [ ] - create new function that will create matrix or add settings to it.
--- defineMinOutliers.R
+++ defineMinOutliers.R
@@ -1,18 +1,18 @@
 defineMinOutliers <- function(x)
 {
   channel1 <- as.numeric(x[,1])
-  hist.data <- hist(channel1, breaks=200, plot=FALSE)
+  hist.data <- hist(channel1, breaks=300, plot=FALSE)
   firstBigCluster <- grep(pattern = max(hist.data$counts), x = hist.data$counts)
   closestZero <- max((1:firstBigCluster)[hist.data$counts[1:firstBigCluster] == 0])
-  MinOulierCh1 <- hist.data$mids[closestZero]
+  MinOutlierCh1 <- hist.data$mids[closestZero]
   
   channel2 <- as.numeric(x[,2])
-  hist.data <- hist(channel2, breaks=200, plot=FALSE)
+  hist.data <- hist(channel2, breaks=300, plot=FALSE)
   firstBigCluster <- grep(pattern = max(hist.data$counts), x = hist.data$counts)
   closestZero <- max((1:firstBigCluster)[hist.data$counts[1:firstBigCluster] == 0])
-  MinOulierCh2 <- hist.data$mids[closestZero]
+  MinOutlierCh2 <- hist.data$mids[closestZero]
   
-  results <- c(MinOulierCh1 = MinOulierCh1, MinOulierCh2 = MinOulierCh2)
+  results <- c(MinOutlierCh1 = MinOutlierCh1, MinOutlierCh2 = MinOutlierCh2)
   
   return(results)
 }
