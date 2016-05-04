1609aca51cd21e6e6d6f86006ee41ae358309300
Dirk van Essen <h.vanessen@vumc.nl>
2016-05-04 22:56:55 +0200

    changes in define cluster function

--- defineClusters.R
+++ defineClusters.R
@@ -1,17 +1,23 @@
-defineClusters <- function(x, thresholds)
+defineClusters <- function(x, tData = NULL)
 {
-  if (length(thresholds) != 2) {
-    stop("thresholds must have a length of 2.\n")
-  } else 
+  if (class(tData) == "matrix") 
     {
-      results <- rep(NA, dim(x)[1])
-      selection <- x[,3] %in% c(0,5)
-      results[x[,1] < thresholds[1] & x[,2] < thresholds[2]] <- 1 # ch1-ch2- : cluster 1
-      results[x[,1] > thresholds[1] & x[,2] < thresholds[2]] <- 2 # ch1+ch2- : cluster 2
-      results[x[,1] > thresholds[1] & x[,2] > thresholds[2]] <- 3 # ch1+ch2+ : cluster 3
-      results[x[,1] < thresholds[1] & x[,2] > thresholds[2]] <- 4 # ch1-ch2+ : cluster 4
-      results[selection] <- x[selection,3]
+    if("threshold" %in% row.names(tData) == TRUE)
+      {
+      threshold <- tData[row.names(tData) %in% "threshold",]
+      }
+    if("thresholdMeanStDev" %in% row.names(tData) == TRUE)
+      {
+      threshold <- tData[row.names(tData) %in% "thresholdMeanStDev",]
     }
-  x[,3] <- results
+    results <- rep(NA, dim(x)[1])
+    selection <- x[,3] %in% c(0,5)
+    results[x[,1] < threshold[1] & x[,2] < threshold[2]] <- 1 # ch1-ch2- : cluster 1
+    results[x[,1] > threshold[1] & x[,2] < threshold[2]] <- 2 # ch1+ch2- : cluster 2
+    results[x[,1] > threshold[1] & x[,2] > threshold[2]] <- 3 # ch1+ch2+ : cluster 3
+    results[x[,1] < threshold[1] & x[,2] > threshold[2]] <- 4 # ch1-ch2+ : cluster 4
+    results[selection] <- x[selection,3]
+    x[,3] <- results
+    }else{stop("Threshold data is not given as a matrix.\n")}
   return(x)
 }
