9dc12b7a28f358731e45c6a2b2b59cdd6a6e27e4
Dirk van Essen <h.vanessen@vumc.nl>
2016-05-05 15:39:53 +0200

    changes to work with 'thresholdData' function

--- plot_ddPCR.R
+++ plot_ddPCR.R
@@ -1,23 +1,34 @@
-plot_ddPCR <- function(x, dotres = 0.7, main = "ddPCR", pch = 16, colors = "ddpcr", density = 60, thresholds = NULL, max.xy = NULL, verbose = FALSE)
+plot_ddPCR <- function(x, dotres = 0.7, main = "ddPCR", pch = 16, colors = "ddpcr", density = 60, verbose = FALSE)
 {
-  if(length(max.xy) != 2) {
+  if("maxAmplitude" %in% row.names(tData) == TRUE) {
+    xmax <- tData[row.names(tData) %in% "maxAmplitude",2]
+    ymax <- tData[row.names(tData) %in% "maxAmplitude",1]
+  } else {
     xmax <- max(x[,2])
     ymax <- max(x[,1])
-  } else {
-    xmax <- max.xy[2]
-    ymax <- max.xy[1]
   }
-  col.vec <- defineColor(x = x[,3], density = density)
+  if(length(colors) == unique(x[,3])) {
+   col.vec <- colors 
+  } else {col.vec <- defineColor(x = x[,3], density = density)}
+  
   plot(y = x[,1], x = x[,2], cex = dotres, col = col.vec, 
        ylab = "Ch1 Amplitude", xlab = "Ch2 Amplitude", 
        pch = pch, main = main,
        xlim = c(0, xmax), ylim = c(0, ymax))
   sub.text <- dropletCountText(x = x[,3])
   mtext(side = 3,text = sub.text, cex = 0.8)
-  if (class(thresholds) != "matrix") {
+  
+  if (class(tData) != "matrix") {
     if(verbose == TRUE){cat("Threshold data must be a matrix. Data will not be plotted.")}
-  }else{
-    abline(h = thresholds[,1], col = "red") # channel 1
-    abline(v = thresholds[,2], col = "red") # channel 2
-  }
+  } else {
+    if("thresholdMeanStDev" %in% row.names(tData) == TRUE) 
+      {
+      abline(v = tData[row.names(tData) %in% "thresholdMeanStDev",2], col = "red")
+      abline(h = tData[row.names(tData) %in% "thresholdMeanStDev",1], col = "red")
+      }else if("threshold" %in% row.names(tData) == TRUE) 
+        {
+      abline(v = tData[row.names(tData) %in% "threshold",2], col = "red")
+      abline(h = tData[row.names(tData) %in% "threshold",1], col = "red")
+        }
+    }
 }
