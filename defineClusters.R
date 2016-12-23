e2abfb75239f0f40392a224e84388026cf92f719
HF van Essen <h.vanessen@vumc.nl>
2016-12-23 16:37:18 +0100

    finished defineTheRain

--- defineClusters.R
+++ defineClusters.R
@@ -1,4 +1,4 @@
-defineClusters <- function(x, tData = NULL)
+defineClusters <- function(x, tData = NULL, outlier = TRUE, rain = TRUE)
 {
   if (class(tData) == "matrix") 
     {
@@ -16,26 +16,30 @@ defineClusters <- function(x, tData = NULL)
     results[x[,1] > threshold[1] & x[,2] < threshold[2]] <- 2 # ch1+ch2- : cluster 2
     results[x[,1] > threshold[1] & x[,2] > threshold[2]] <- 3 # ch1+ch2+ : cluster 3
     results[x[,1] < threshold[1] & x[,2] > threshold[2]] <- 4 # ch1-ch2+ : cluster 4
-    
-    if("minOutlier" %in% row.names(tData) == TRUE)
-    {
-      minOutlier <- tData[row.names(tData) %in% "minOutlier",]
-      results[x[,1] < minOutlier[1] & x[,2] < threshold[2]] <- 0 
-      results[x[,2] < minOutlier[2] & x[,1] < threshold[1]] <- 0 # minOutlier : cluster 0
+    if (ourlier == TRUE){
+      if("minOutlier" %in% row.names(tData) == TRUE)
+      {
+        minOutlier <- tData[row.names(tData) %in% "minOutlier",]
+        results[x[,1] < minOutlier[1] & x[,2] < threshold[2]] <- 0 
+        results[x[,2] < minOutlier[2] & x[,1] < threshold[1]] <- 0 # minOutlier : cluster 0
+      }
+      if("maxOutlier" %in% row.names(tData) == TRUE)
+      {
+        maxOutlier <- tData[row.names(tData) %in% "maxOutlier",]
+        results[x[,1] > maxOutlier[1]] <- 0
+        results[x[,2] > maxOutlier[2]] <- 0 # maxOutlier : cluster 0
+      }
     }
-    if("maxOutlier" %in% row.names(tData) == TRUE)
-    {
-      maxOutlier <- tData[row.names(tData) %in% "maxOutlier",]
-      results[x[,1] > maxOutlier[1]] <- 0
-      results[x[,2] > maxOutlier[2]] <- 0 # maxOutlier : cluster 0
+    if (rain == TRUE){
+      if("minRain" %in% row.names(tData) == TRUE & "maxRain" %in% row.names(tData) == TRUE)
+      {
+        minRain <- tData[row.names(tData) %in% "minRain",]
+        maxRain <- tData[row.names(tData) %in% "maxRain",]
+        results[x[,1] > minRain[1] & x[,1] < maxRain[1] & (results == 1 | results == 2) ] <- 5 # Rain ch1 : cluster 5
+        results[x[,2] > minRain[2] & x[,2] < maxRain[2] & (results == 1 | results == 4) ] <- 5 # Rain ch2 : cluster 5
+      }
     }
-    if("minRain" %in% row.names(tData) == TRUE & "maxRain" %in% row.names(tData) == TRUE)
-    {
-      minRain <- tData[row.names(tData) %in% "minRain",]
-      maxRain <- tData[row.names(tData) %in% "maxRain",]
-      results[x[,1] > minRain[1] & x[,1] < maxRain[1] & (results == 1 | results == 2) ] <- 5
-      results[x[,2] > minRain[2] & x[,2] < maxRain[2] & (results == 1 | results == 4) ] <- 5 # Rain : cluster 5
-    }
+
     x[,3] <- results
     }else{stop("Threshold data is not given as a matrix.\n")}
   return(x)
--- defineTheRain.R
+++ defineTheRain.R
@@ -1,4 +1,4 @@
-defineTheRain <- function(x, stdev = 6, tData = NULL)
+defineTheRain <- function(x, stdev = 3, tData = NULL)
 {
   if (class(tData) == "matrix") {
     if("thresholdMeanStDev" %in% row.names(tData) == TRUE){
@@ -8,17 +8,17 @@ defineTheRain <- function(x, stdev = 6, tData = NULL)
         threshold <- tData[row.names(tData) %in% "threshold",]
       }else {stop("Threshold data is not given.\n")}
     }
-    
+    x <- defineClusters(x = x, tData = tData)
     rainResult <- matrix(data = NA, nrow = 2, ncol = 2, 
                          dimnames = list(c("minRain", "maxRain"), c("ch1.Amplitude","ch2.Amplitude")), 
                          byrow = TRUE)
-    ch1Low <- calculateMeanSdCluster(x, cluster = c(1,4), stdev = stdev)
+    ch1Low <- calculateMeanSdCluster(x, cluster = c(1), stdev = stdev)
     rainResult[1,1] <- ch1Low[1,1] + ch1Low[2,1]
-    ch1High <- calculateMeanSdCluster(x, cluster = c(2,3), stdev = stdev)
+    ch1High <- calculateMeanSdCluster(x, cluster = c(2), stdev = stdev)
     rainResult[2,1] <- ch1High[1,1] - ch1High[2,1]
-    ch2Low <- calculateMeanSdCluster(x, cluster = c(1,2), stdev = stdev)
+    ch2Low <- calculateMeanSdCluster(x, cluster = c(1), stdev = stdev)
     rainResult[1,2] <- ch2Low[1,2] + ch2Low[2,2]
-    ch2High <- calculateMeanSdCluster(x, cluster = c(3,4), stdev = stdev)
+    ch2High <- calculateMeanSdCluster(x, cluster = c(4), stdev = stdev)
     rainResult[2,2] <- ch2High[1,2] - ch2High[2,2]
     
     tData <- thresholdData(tData = tData, amplitude = rainResult[1,], type = 'minRain')
