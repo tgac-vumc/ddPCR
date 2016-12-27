4576f70bd1ae18df6b9efdb85b3c755b5b3b00ab
HF van Essen <h.vanessen@vumc.nl>
2016-12-27 14:33:56 +0100

    making stuff better

--- ddPCR.R
+++ ddPCR.R
@@ -1,6 +1,15 @@
 # PACKAGES
-library(dplyr)
-library(magrittr)
-#library(dpcR)
+
+if("dplyr" %in% installed.packages()[,1] == FALSE){
+  install.packages("dplyer")
+} else { library(dplyr) }
+if("magrittr" %in% installed.packages()[,1] == FALSE){
+  install.packages("magrittr")
+} else { library(magrittr) }
+if("xlsx" %in% installed.packages()[,1] == FALSE){
+  install.packages("xlsx")
+} else { library(xlsx) }
+
+#library(ddpcR)
 # end, HF van Essen 2016
   
