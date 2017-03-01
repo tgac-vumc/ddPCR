2bd28da748879e890c763dd1a4cdad069480fc47
HF van Essen <h.vanessen@vumc.nl>
2017-03-01 15:45:47 +0100

    minor changes in plot function

--- plot_ddPCR.R
+++ plot_ddPCR.R
@@ -9,19 +9,18 @@ plot_ddPCR <- function(x = NULL, dotres = 0.7, tData = NULL, main = "ddPCR", pch
         if(verbose == TRUE){cat("Reading Threshold data.\n")}
         xmax <- tData[row.names(tData) %in% "maxAmplitude",2]
         ymax <- tData[row.names(tData) %in% "maxAmplitude",1]
-      } else {
-        if(verbose == TRUE){cat("Amplitude data not found. tData will not be used.\n")}
-        xmax <- max(x[,2])
-        ymax <- max(x[,1])
-      }
+      } 
     } else {
-      if(verbose == TRUE){cat("Threshold data must be a matrix. tData will not be used.\n")}
+      if(verbose == TRUE){cat("Amplitude data not found. tData will not be used.\n")}
       xmax <- max(x[,2])
       ymax <- max(x[,1])
-     
     }
-  } 
-  
+  } else {
+    if(verbose == TRUE){cat("Threshold data must be a matrix. tData will not be used.\n")}
+    xmax <- max(x[,2])
+    ymax <- max(x[,1])
+    }
+   
   col.vec <- defineColor(x = x[,3], density = density)
   plot(y = x[,1], x = x[,2], cex = dotres, col = col.vec, 
        ylab = "Ch1 Amplitude", xlab = "Ch2 Amplitude", 
