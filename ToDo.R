923e35d514c6df17a5353d0d7b929a2d26b9a456
Dirk van Essen <h.vanessen@vumc.nl>
2017-11-25 01:52:03 +0100

    to do update

--- ToDo.R
+++ ToDo.R
@@ -8,6 +8,22 @@
 # [ ] update clusters after setting defineTheRain
 # [ ] update colors after setting defineTheRain
 # [x] add probe name to the plot
-# [ ] update overview with correct sample locations as row names
-# [ ] determine the best amount of breaks for densityhist
+# [x] update overview with correct sample locations as row names
+# [ ] add Cluster 1 mean to 'overview' function
+# [ ] fix bug 'minOutliers'
+# [ ] determine the best amount of breaks for densityhist -> works good >= 25
 # [ ] integrate mean + 3sd
+# [ ] integrate 'outliers' package
+
+
+### idea new to remove outliers?
+# [ ] mean first big cluster, 
+# [ ] mirror everything left to right side = neg cluster
+# [ ] take mean + 2.5sd or 3sd to determine the edges
+# [ ] all droplets < edge = minOutlier
+
+# or use 'outliers'
+library(outliers)
+# select negative ch1
+# select negative ch2
+# outliers in both -> if true in both... remove
