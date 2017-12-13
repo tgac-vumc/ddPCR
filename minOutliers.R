340e62529c0c70c05e069fa60cd2eae54618be0a
Dirk van Essen <h.vanessen@vumc.nl>
2017-12-13 22:13:37 +0100

    updated minOutliers() function

--- minOutliers.R
+++ minOutliers.R
@@ -20,77 +20,38 @@
   x <- x[selection,]
   return(x)
 }
-minOutliers <- function(data = NULL, well = NULL, 
-                        breaks = 250, strict = TRUE,
-                        verbose = TRUE){
-    if((class(data)[1] == "ddPCRdata") != TRUE){
-      stop ("data structure is not in the correct format.\n\n")
-    }
-    if(verbose == TRUE){
-      cat("Checking for outliers in each sample.\n")
-    }
-    if(is.null(well) == TRUE){
-      wells <- 1:ncol(data@phenoData$sampleData)
-    } else {
-      wells <- well
-    }
+minOutliers <- function(data = NULL, verbose = TRUE){
+  if((class(data)[1] == "ddPCRdata") != TRUE){
+    stop ("data structure is not in the correct format.\n")
+  }
+  if(verbose == TRUE){
+    cat("Processing samples for outliers in the lower ranges.\n")
+  }
+  samples <- data@phenoData$sampleData['name',]
+  for(i in 1:length(samples)){
+    ### ANALYSIS CHANNEL 1
+    channel1 <- as.numeric(data@assayData$Ch1.Amplitude[,i])
+    hist.data <- hist(channel1, breaks=300, plot=FALSE)
+    firstBigClusterCh1 <- grep(pattern = max(hist.data$counts), x = hist.data$counts)
     
-  for(z in 1:length(wells)){
-    well <- wells[z]
-    ### analyzing channel 1
-    ch1 <- data@assayData$Ch1.Amplitude[ ,well]
-    x <- ch1[!(ch1 %in% NA)]
-    x <- x[x < data@phenoData$ch1['threshold', well]]
-    
-    if(strict == TRUE){
-      breaks.ch1 <- .makeBreaks(min = min(x), max = max(x), breaks = breaks)
-    } else {
-      breaks.ch1 <- breaks
+    if(TRUE %in% (hist.data$counts[1:firstBigClusterCh1] %in% 0 == TRUE)){
+      closestZero <- max((1:firstBigClusterCh1)[hist.data$counts[1:firstBigClusterCh1] == 0])
+      data@phenoData$ch1['minOutlier',i] <- hist.data$mids[closestZero]
+    } else  {data@phenoData$ch1['minOutlier',i] <- hist.data$breaks[1] - diff(hist.data$breaks[1:2]) 
+    if(data@phenoData$ch1['minOutlier',i] < 0){data@phenoData$ch1['minOutlier',i] <- 0 }
     }
-    hist.data <- hist(x, breaks = breaks.ch1, plot = FALSE)
-    x <- hist.data$counts
-    result <- NULL
-    for(i in 1:(length(x)-5)){
-      if(x[i] > x[i+1] & x[i+1] <= x[i+2] &
-         x[i+2] >= x[i+3] & x[i+3] >= x[i+4]) {
-        result <- c(result, (i+1))
-      }
-    }
+    ### ANALYSIS CHANNEL 2
+    channel2 <- as.numeric(data@assayData$Ch2.Amplitude[,i])
+    hist.data <- hist(channel2, breaks=300, plot=FALSE)
+    firstBigClusterCh2 <- grep(pattern = max(hist.data$counts), x = hist.data$counts)
     
-    
-    if(is.null(result) != TRUE){
-      result <- result[1]
-      result.ch1 <- hist.data$mids[result]
-      
-      ### analyzing channel 2
-      ch2 <- data@assayData$Ch2.Amplitude[ ,well]
-      x <- ch2[!(ch2 %in% NA)]
-      x <- x[x < data@phenoData$ch2['threshold', well]]
-      
-      if(strict == TRUE){
-        breaks.ch2 <- .makeBreaks(min = min(x), max = max(x), breaks = breaks)
-      } else {
-        breaks.ch2 <- breaks
-      }
-      hist.data <- hist(x, breaks = breaks.ch2, plot = FALSE)
-      x <- hist.data$counts
-      result <- NULL
-      for(i in 1:(length(x)-5)){
-        if(x[i] > x[i+1] & x[i+1] <= x[i+2] &
-           x[i+2] >= x[i+3] & x[i+3] >= x[i+4]) {
-          result <- c(result, (i+1))
-        }
-      }
-      
-      if(is.null(result) != TRUE){
-        result <- result[1]
-        result.ch2 <- hist.data$mids[result]
-        
-        data@phenoData$ch1['minOutlier', well] <- result.ch1
-        data@phenoData$ch2['minOutlier', well] <- result.ch2
-      }
+    if(TRUE %in% (hist.data$counts[1:firstBigClusterCh2] %in% 0 == TRUE)){
+      closestZero <- max((1:firstBigClusterCh2)[hist.data$counts[1:firstBigClusterCh2] == 0])
+      data@phenoData$ch2['minOutlier',i] <- hist.data$mids[closestZero]
+    } else {data@phenoData$ch2['minOutlier',i] <- hist.data$breaks[1] - diff(hist.data$breaks[1:2]) 
+    if(data@phenoData$ch2['minOutlier',i] < 0){data@phenoData$ch2['minOutlier',i] <- 0 }
     }
   }
-    data <- .updateClusters(data)
-    return(data)
+  data <- .updateClusters(data)
+  return(data)
 }
